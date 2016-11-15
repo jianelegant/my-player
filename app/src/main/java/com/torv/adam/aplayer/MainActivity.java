f36bf702f2e2bcf4a49acc38f1a9109abc126abf
jianelegant <jian.elegant@gmail.com>
2016-11-15 23:14:12 +0800

    play video via VideoView

--- app/src/main/java/com/torv/adam/aplayer/MainActivity.java
+++ app/src/main/java/com/torv/adam/aplayer/MainActivity.java
@@ -1,13 +1,35 @@
 package com.torv.adam.aplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.torv.adam.aplayer.videoview.VideoViewActivity;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.id_videoview).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, VideoViewActivity.class));
+            }
+        });
+
+        findViewById(R.id.id_mediacodec).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
