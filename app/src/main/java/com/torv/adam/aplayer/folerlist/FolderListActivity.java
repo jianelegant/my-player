a4ae05fc0fb17033d0ba4779da3e109a53b9f9e7
jianelegant <jian.elegant@gmail.com>
2016-11-22 19:40:06 +0800

    perfect the premission check logic

--- app/src/main/java/com/torv/adam/aplayer/folerlist/FolderListActivity.java
+++ app/src/main/java/com/torv/adam/aplayer/folerlist/FolderListActivity.java
@@ -1,9 +1,16 @@
 package com.torv.adam.aplayer.folerlist;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.torv.adam.aplayer.R;
 import com.torv.adam.aplayer.utils.PermissionUtil;
@@ -16,11 +23,14 @@ public class FolderListActivity extends AppCompatActivity {
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
 
+    private TextView mDenyHint;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folder_list);
 
+        mDenyHint = (TextView) findViewById(R.id.id_deny_hint);
         checkPermission();
     }
 
@@ -29,15 +39,40 @@ public class FolderListActivity extends AppCompatActivity {
             PermissionUtil.checkNeedShowRationable(FolderListActivity.this, PERMISSIONS, new PermissionUtil.IRationableResultCallback() {
                 @Override
                 public void onNeedShowRationablePermissions(List<String> rationablePermisssions) {
-                    // TODO explain dialog
+                    if(rationablePermisssions.contains(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(FolderListActivity.this);
+                        builder.setTitle(R.string.permission_rational_title);
+                        builder.setMessage(R.string.permission_rational_msg);
+                        builder.setCancelable(false);
+                        builder.setNegativeButton(R.string.permission_rational_cancel, null);
+                        builder.setPositiveButton(R.string.permission_rational_grant, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                PermissionUtil.requestPermission(FolderListActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE});
+                            }
+                        });
+                        builder.show();
+                    }
                 }
 
                 @Override
                 public void onNeedRequestPermissions(List<String> needRequestPermisssions) {
-                    PermissionUtil.requestPermission(FolderListActivity.this, needRequestPermisssions.toArray(new String[needRequestPermisssions.size()]));
+                    if(needRequestPermisssions.size() > 0) {
+                        PermissionUtil.requestPermission(FolderListActivity.this, needRequestPermisssions.toArray(new String[needRequestPermisssions.size()]));
+                    }
                 }
             });
+        } else {
+            mDenyHint.setVisibility(View.GONE);
+            replaceFragment();
         }
+    }
+
+    private void replaceFragment(){
+        Fragment newFragment = new FolderListFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.id_frg_holder, newFragment);
+        transaction.commit();
     }
 
     @Override
@@ -46,12 +81,17 @@ public class FolderListActivity extends AppCompatActivity {
         PermissionUtil.handleGrantResult(requestCode, permissions, grantResults, new PermissionUtil.IGrantResultCallback() {
             @Override
             public void onGrantedPermissions(List<String> grantedPermissions) {
-                // TODO
+                if(null != grantedPermissions && grantedPermissions.contains(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    mDenyHint.setVisibility(View.GONE);
+                    replaceFragment();
+                }
             }
 
             @Override
             public void onDeniedPermissions(List<String> deniedPermissions) {
-                // TODO
+                if(null != deniedPermissions && deniedPermissions.contains(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    Toast.makeText(FolderListActivity.this, R.string.permission_deny_hint, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
